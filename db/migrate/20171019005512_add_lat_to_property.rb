675565b03d59d5164488e195088be16778c0dbb5
puertos <diego.fissina@fluxit.com.ar>
2017-11-14 17:18:23 -0300

    lat float(10,8) y lng float(11,8)

--- db/migrate/20171019005512_add_lat_to_property.rb
+++ db/migrate/20171019005512_add_lat_to_property.rb
@@ -1,5 +1,5 @@
 class AddLatToProperty < ActiveRecord::Migration[5.1]
   def change
-    add_column :properties, :lat, :float
+    add_column :properties, :lat, :float, :precision => 15, :scale => 8
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@ ActiveRecord::Schema.define(version: 20171103193344) do
 
   create_table "comunas", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci" do |t|
     t.string "name"
-    t.float "lat", limit: 24
-    t.float "lng", limit: 24
+    t.float "lat", precision: 10, scale: 8
+    t.float "lng", precision: 11, scale: 8
     t.bigint "region_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -92,8 +92,8 @@ ActiveRecord::Schema.define(version: 20171103193344) do
     t.decimal "expenses", precision: 10
     t.boolean "pets"
     t.boolean "terrace"
-    t.decimal "lat", precision: 10, scale: 8
-    t.decimal "lng", precision: 11, scale: 8
+    t.float "lat", precision: 10, scale: 8
+    t.float "lng", precision: 11, scale: 8
     t.string "cod"
     t.integer "views", default: 0, null: false
     t.boolean "active", default: true, null: false
@@ -112,8 +112,8 @@ ActiveRecord::Schema.define(version: 20171103193344) do
 
   create_table "regions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci" do |t|
     t.string "name"
-    t.float "lat", limit: 24
-    t.float "lng", limit: 24
+    t.float "lat", precision: 10, scale: 8
+    t.float "lng", precision: 11, scale: 8
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
