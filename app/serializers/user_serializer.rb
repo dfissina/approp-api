87bfdd0e5b84f81b89b52506a3029e8d5f304a21
dfissina <dfissina@gmail.com>
2017-12-12 21:41:12 -0300

    User serializer add facebook_account

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   
-  attributes :id, :first_name, :last_name, :email, :phone, :cell_phone, :profile_picture
+  attributes :id, :first_name, :last_name, :email, :phone, :cell_phone, :profile_picture, :facebook_account
   
   def profile_picture
     object.profile_picture.url
